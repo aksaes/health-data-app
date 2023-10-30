f9d04d51474aa0eb35ba9237c28bdc13b7279145
Aksa Elizabeth Sunny <aksaelizabeth28@gmail.com>
2023-10-30 12:48:03 -0700

    Create Template for TrafficDataActivity

---
+++ app/src/main/java/com/example/healthmonitor/activities/TrafficDataActivity.java
@@ -0,0 +1,14 @@
+package com.example.healthmonitor.activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+import com.example.healthmonitor.R;
+
+public class TrafficDataActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_traffic_data);
+    }
+}
